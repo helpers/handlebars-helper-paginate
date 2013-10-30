ce8f19db18aa7367d9d345a8d83060d8dbfab4a4
Jon Schlinkert <dev@sellside.com>
2013-10-30 06:59:12 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# {{paginate}} [![NPM version](https://badge.fury.io/js/helper-paginate.png)](http://badge.fury.io/js/helper-paginate)
+
+> {{paginate}} handlebars helper for Assemble.
+
+See [live examples →](http://assemble.github.io/example-pagination/)
+
+
+## Quickstart
+In the root of your project, run the following in the command line:
+
+```bash
+npm i handlebars-helper-paginate --save-dev
+```
+Please [create an issue](https://github.com/helpers/handlebars-helper-paginate/issues/new) to report bugs or to make a feature request.
+
+
+## Assemble config
+If you use [Assemble config](http://assemble.io) and Grunt, in your Gruntfile simply add `handlebars-helper-paginate` to the `helpers` property in the [Assemble](http://assemble.io) task or target options:
+
+```javascript
+grunt.initConfig({
+  assemble: {
+    options: {
+      // the 'handlebars-helper-paginate' module must also be listed in devDependencies
+      // for assemble to automatically resolve the helper
+      helpers: ['handlebars-helper-paginate', 'foo/*.js']
+    }
+    ...
+  }
+});
+```
+
+You can now use begin using the helper in your templates:
+
+```handlebars
+{{paginate pagination}}
+```
+Results in:
+
+```html
+<ul class="pager">
+  <li class="previous">
+    <a href="index.html">&larr; Previous</a>
+  </li>
+  <li> <a href="index.html">1</a> </li>
+  <li class="active">
+    <a href="foo-01.html">2</a>
+  </li>
+  <li> <a href="foo-02.html">3</a> </li>
+  <li> <a href="foo-03.html">4</a> </li>
+  <li> <a href="foo-04.html">5</a> </li>
+  <li> <a href="foo-05.html">6</a> </li>
+  <li class="next">
+    <a href="foo-02.html">Next &rarr;</a>
+  </li>
+</ul>
+```
+Example:
+
+![image](https://f.cloud.github.com/assets/383994/1436000/b8fd1968-414c-11e3-9019-90e3106ead76.png)
+
+See [live examples](http://assemble.github.io/example-pagination/).
+
+## Options
+
+Coming soon... I plan to make this helper more customizable, please [create an issue](https://github.com/helpers/handlebars-helper-paginate/issues/new) to report bugs or to make a feature request.
+
+
+## Author
+
++ [github/Jon Schlinkert](http://github.com/Jon Schlinkert)
++ [twitter/Jon Schlinkert](http://twitter.com/Jon Schlinkert)
+
+
+## License and Copyright
+
+Licensed under the [MIT License](./LICENSE-MIT)
+Copyright (c) Jon Schlinkert, contributors.
---
+++ index.js
@@ -0,0 +1,72 @@
+/**
+ * Handlebars Helpers: {{paginate}}
+ * Copyright (c) 2013 Jon Schlinkert
+ * Licensed under the MIT License (MIT).
+ */
+
+'use strict';
+
+// Node.js
+var path = require('path');
+var fs = require('fs');
+
+// node_modules
+var _     = require('lodash');
+
+// Export helpers
+module.exports.register = function (Handlebars, options, params) {
+
+  var opts = options || {};
+
+  /**
+   * {{paginate}}
+   * Adds a pager to enable navigating to prev and next page/post.
+   * @param  {Object} context Context to pass to the helper, most likely `pagination`.
+   * @param  {Object} options Pass a modifier class to the helper.
+   * @return {String}         The pager, HTML.
+   */
+  Handlebars.registerHelper('paginate', function(context, modifier, options) {
+
+    options = options || {};
+    options.hash = options.hash || {};
+    context = _.extend({modifier: ''}, context, opts, this, options.hash);
+
+    var template = [
+      '<ul class="pager {{modifier}}">',
+      // '  {{#is pagination.currentPage 1}}',
+      // '    <li class="pager-heading">POPULAR</li>',
+      // '  {{/is}}',
+      '  {{#is pagination.currentPage 1}}',
+      '    <li class="previous">',
+      '      <a href="{{relative page.dest "./_gh_pages/index.html"}}">&larr; Home</a>',
+      '    </li>',
+      '  {{/is}}',
+      '  {{#isnt pagination.currentPage 1}}',
+      '    <li class="previous">',
+      '      <a href="{{relative page.dest prev.dest}}">&larr; Previous</a>',
+      '    </li>',
+      '  {{/isnt}}',
+      '',
+      '  {{#eachItems pages}}',
+      '    <li{{#is ../page.dest this.dest}} class="active"{{/is}}>',
+      '      <a href="{{relative ../page.dest this.dest}}">{{@number}}</a>',
+      '    </li>',
+      '  {{/eachItems}}',
+      '',
+      '  {{#isnt pagination.currentPage pagination.totalPages}}',
+      '    <li class="next">',
+      '      <a href="{{relative page.dest next.dest}}">Next &rarr;</a>',
+      '    </li>',
+      '  {{/isnt}}',
+      '  {{#is pagination.currentPage pagination.totalPages}}',
+      '    <li class="next disabled">',
+      '      <a href="{{relative page.dest next.dest}}">Next &rarr;</a>',
+      '    </li>',
+      '  {{/is}}',
+      '</ul>'
+    ].join('\n');
+
+    return new Handlebars.SafeString(Handlebars.compile(template)(context));
+  });
+
+};
