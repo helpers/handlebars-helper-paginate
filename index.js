b7e5c8dca6a3e39c76581425646f1c1a502e937a
Jon Schlinkert <dev@sellside.com>
2013-11-28 14:53:22 -0500

    bumped to v0.2.0:
    * adds new pagination helpers
    * adds lodash as a dependency given the recent changes to Grunt

--- index.js
+++ index.js
@@ -10,11 +10,88 @@
 var path = require('path');
 var fs = require('fs');
 
+
 // Export helpers
 module.exports.register = function (Handlebars, options, params) {
 
   var opts = options || {};
-  var _ = params.grunt.util._;
+  var _ = require('lodash');
+
+
+  /**
+   * {{pager}}
+   * Adds a pager to enable navigating to prev and next page/post.
+   * @param  {Object} context Context to pass to the helper, most likely `pagination`.
+   * @param  {Object} options Pass a modifier class to the helper.
+   * @return {String}         The pager, HTML.
+   */
+  exports.pager = function(context, options) {
+    options = options || {};
+    options.hash = options.hash || {};
+    context = _.extend({modifier: ''}, context, opts.data, this, options.hash);
+
+    var template = [
+      '<ul class="nav nav-pills nav-stacked">',
+      '',
+      '  {{#is pagination.currentPage 1}}',
+      '    <li class="prev disabled">',
+      '      <a unselectable="on" class="unselectable">First</a>',
+      '    </li>',
+      '    <li class="prev disabled">',
+      '      <a unselectable="on" class="unselectable">&larr; Previous</a>',
+      '    </li>',
+      '  {{/is}}',
+      '',
+      '  {{#not pagination.currentPage 1}}',
+      '    <li class="prev">',
+      '      <a href="{{relative page.dest first.dest}}">First</a>',
+      '    </li>',
+      '    <li class="prev">',
+      '      <a href="{{relative page.dest prev.dest}}">&larr; Previous</a>',
+      '    </li>',
+      '  {{/not}}',
+      '',
+      '  {{#eachItems pages}}',
+      '    <li{{#is ../page.dest this.dest}} class="active"{{/is}}>',
+      '      <a href="{{relative ../page.dest this.dest}}">{{@number}}</a>',
+      '    </li>',
+      '  {{/eachItems}}',
+      '',
+      '  {{#not pagination.currentPage pagination.totalPages}}',
+      '    <li class="next">',
+      '      <a href="{{relative page.dest next.dest}}">Next &rarr;</a>',
+      '    </li>',
+      '    <li class="next">',
+      '      <a href="{{relative page.dest last.dest}}">Last</a>',
+      '    </li>',
+      '  {{/not}}',
+      '',
+      '  {{#is pagination.currentPage pagination.totalPages}}',
+      '    <li class="next disabled">',
+      '      <a unselectable="on" class="unselectable">Next &rarr;</a>',
+      '    </li>',
+      '    <li class="next disabled">',
+      '      <a unselectable="on" class="unselectable">Last</a>',
+      '    </li>',
+      '  {{/is}}',
+      '',
+      '</ul>'
+    ].join('\n');
+
+    var styles = [
+      '<style>',
+        '.unselectable {',
+        '  -webkit-touch-callout: none;',
+        '    -webkit-user-select: none;',
+        '       -moz-user-select: none;',
+        '        -ms-user-select: none;',
+        '            user-select: none;',
+        '}',
+      '</style>'
+    ].join('\n');
+
+    return new Handlebars.SafeString(Handlebars.compile(template)(context) + styles);
+  };
 
   /**
    * {{paginate}}
@@ -24,21 +101,22 @@ module.exports.register = function (Handlebars, options, params) {
    * @return {String}         The pager, HTML.
    */
   exports.paginate = function(context, options) {
-
     options = options || {};
     options.hash = options.hash || {};
     context = _.extend({modifier: ''}, context, opts.data, this, options.hash);
 
     var template = [
-      '<ul class="pager {{modifier}}">',
+      '<ul class="pager{{#if modifier}} {{modifier}}{{/if}}">',
+      '',
       '  {{#is pagination.currentPage 1}}',
       '    <li class="previous disabled">',
-      '      <a href="{{relative page.dest prev.dest}}">&larr; Prev</a>',
+      '      <a unselectable="on" class="unselectable">&larr; Prev</a>',
       '    </li>',
       '  {{/is}}',
+      '',
       '  {{#isnt pagination.currentPage 1}}',
       '    <li class="previous">',
-      '      <a href="{{relative page.dest prev.dest}}">&larr; Prev</a>',
+      '      <a href="{{relative page.dest first.dest}}">&larr; Prev</a>',
       '    </li>',
       '  {{/isnt}}',
       '',
@@ -50,18 +128,109 @@ module.exports.register = function (Handlebars, options, params) {
       '',
       '  {{#isnt pagination.currentPage pagination.totalPages}}',
       '    <li class="next">',
-      '      <a href="{{relative page.dest next.dest}}">Next &rarr;</a>',
+      '      <a href="{{relative page.dest last.dest}}">Next &rarr;</a>',
       '    </li>',
       '  {{/isnt}}',
+      '',
       '  {{#is pagination.currentPage pagination.totalPages}}',
       '    <li class="next disabled">',
-      '      <a href="{{relative page.dest next.dest}}">Next &rarr;</a>',
+      '      <a unselectable="on" class="unselectable">Next &rarr;</a>',
       '    </li>',
       '  {{/is}}',
+      '',
       '</ul>'
     ].join('\n');
-    return new Handlebars.SafeString(Handlebars.compile(template)(context));
+
+    var styles = [
+      '<style>',
+        '.unselectable {',
+        '  -webkit-touch-callout: none;',
+        '    -webkit-user-select: none;',
+        '       -moz-user-select: none;',
+        '        -ms-user-select: none;',
+        '            user-select: none;',
+        '}',
+      '</style>'
+    ].join('\n');
+    return new Handlebars.SafeString(Handlebars.compile(template)(context) + styles);
   };
+
+
+  /**
+   * {{pagination}}
+   * Adds a pagination to enable navigating to prev and next page/post.
+   * @param  {Object} context Context to pass to the helper, most likely `pagination`.
+   * @param  {Object} options Pass a modifier class to the helper.
+   * @return {String}         The pagination, HTML.
+   */
+  exports.pagination = function(context, options) {
+    options = options || {};
+    options.hash = options.hash || {};
+    context = _.extend({modifier: ''}, context, opts.data, this, options.hash);
+
+    var template = [
+      '<ul class="pagination{{#if modifier}} {{modifier}}{{/if}}">',
+      '',
+      '  {{#is pagination.currentPage 1}}',
+      '    <li class="prev disabled">',
+      '      <a unselectable="on" class="unselectable">&laquo;</a>',
+      '    </li>',
+      '    <li class="prev disabled">',
+      '      <a unselectable="on" class="unselectable">&lsaquo;</a>',
+      '    </li>',
+      '  {{/is}}',
+      '',
+      '  {{#not pagination.currentPage 1}}',
+      '    <li class="prev">',
+      '      <a href="{{relative page.dest first.dest}}">&laquo;</a>',
+      '    </li>',
+      '    <li class="prev">',
+      '      <a href="{{relative page.dest prev.dest}}">&lsaquo;</a>',
+      '    </li>',
+      '  {{/not}}',
+      '',
+      '  {{#eachItems pages}}',
+      '    <li{{#is ../page.dest this.dest}} class="active"{{/is}}>',
+      '      <a href="{{relative ../page.dest this.dest}}">{{@number}}</a>',
+      '    </li>',
+      '  {{/eachItems}}',
+      '',
+      '  {{#not pagination.currentPage pagination.totalPages}}',
+      '    <li class="next">',
+      '      <a href="{{relative page.dest next.dest}}">&rsaquo;</a>',
+      '    </li>',
+      '    <li class="next">',
+      '      <a href="{{relative page.dest last.dest}}">&raquo;</a>',
+      '    </li>',
+      '  {{/not}}',
+      '',
+      '  {{#is pagination.currentPage pagination.totalPages}}',
+      '    <li class="next disabled">',
+      '      <a unselectable="on" class="unselectable">&rsaquo;</a>',
+      '    </li>',
+      '    <li class="next disabled">',
+      '      <a unselectable="on" class="unselectable">&raquo;</a>',
+      '    </li>',
+      '  {{/is}}',
+      '',
+      '</ul>'
+    ].join('\n');
+
+    var styles = [
+      '<style>',
+        '.unselectable {',
+        '  -webkit-touch-callout: none;',
+        '    -webkit-user-select: none;',
+        '       -moz-user-select: none;',
+        '        -ms-user-select: none;',
+        '            user-select: none;',
+        '}',
+      '</style>'
+    ].join('\n');
+
+    return new Handlebars.SafeString(Handlebars.compile(template)(context) + styles);
+  };
+
 
   for (var helper in exports) {
     if (exports.hasOwnProperty(helper)) {
